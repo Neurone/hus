93346317b6721e8e1740e01a511d05a0fc9d8123
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2020-05-10 15:35:39 +0200

    Added support for new pages + some UI improvements
    - Added support for "DHCP Clients List" page and "ARP List" page
    - Now MAC address is side by side with nickname

--- resolveMacAddresses.user.js
+++ resolveMacAddresses.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Resolve MAC Addresses for TD-W8970 v1 web pages
 // @namespace    https://github.com/Neurone/hus
-// @version      1.0
+// @version      1.1
 // @description  Resolve MAC Addresses based on settings in Wireless MAC Filtering page. Tested on TD-W8970 v1 Firmware Version:0.6.0 2.12 v000c.0 Build 140613 Rel.31066n
 // @author       Giuseppe Bertone <bertone.giuseppe@gmail.com>
 // @match        http://192.168.1.1
@@ -19,10 +19,19 @@
 
     function isEmpty(obj) {
         for (var key in obj) {
-            if (obj.hasOwnProperty(key))
-                return false;
+            if (obj.hasOwnProperty(key)) return false;
         }
         return true;
+    }
+
+    function isMacAddress(string) {
+        return (17 === string.length);
+    }
+
+    function formatCellWithNickname(cell) {
+        let macAddress = cell.text();
+        let deviceNickname = macNames[macAddress] ? macNames[macAddress] : "";
+        if(isMacAddress(macAddress)) cell.html(macAddress + "<br/>" + deviceNickname);
     }
 
     var checkPage = function () {
@@ -34,8 +43,28 @@
                 // Resolve MAC names
                 jQuery("#staTbl tr").each(function () {
                     let cell = jQuery(this).find("td:eq(1)");
-                    let macAddress = cell.text();
-                    cell.text(macNames[macAddress]);
+                    formatCellWithNickname(cell);
+                });
+            }
+        } else if ('DHCP Clients List' === pageName) {
+            if (isEmpty(macNames)) {
+                jQuery("#t_info").html("This page displays the information of DHCP clients. <span style='color:red;font-weight:bold'>To init the MAC address resolution please visit the 'Wireless MAC filtering' page at least once.</span>");
+            } else {
+                // Resolve MAC names
+                jQuery("#hostTbl tr").each(function () {
+                    let cell = jQuery(this).find("td:eq(2)");
+                    formatCellWithNickname(cell);
+                });
+            }
+        } else if ('ARP List' === pageName) {
+            if (isEmpty(macNames)) {
+                let firstChild = jQuery(".con2 p:first");
+                if(firstChild.hasClass("br")) firstChild.before('<p class="L1 T" id="t_info"><span style="color:red;font-weight:bold">To init the MAC address resolution please visit the \'Wireless MAC filtering\' page at least once.</span></p>');
+            } else {
+                // Resolve MAC names
+                jQuery("#arptbl tr").each(function () {
+                    let cell = jQuery(this).find("td:eq(1)");
+                    formatCellWithNickname(cell);
                 });
             }
         } else if ('Wireless MAC Filtering settings' === pageName) {
