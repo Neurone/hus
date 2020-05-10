210fb4f19f526258a71b82040b6e9f0798995745
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2020-05-10 16:17:13 +0200

    Update resolveMacAddresses.user.js

--- resolveMacAddresses.user.js
+++ resolveMacAddresses.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/Neurone/hus
 // @version      1.1
 // @description  Resolve MAC Addresses based on settings in Wireless MAC Filtering page. Tested on TD-W8970 v1 Firmware Version:0.6.0 2.12 v000c.0 Build 140613 Rel.31066n
-// @author       Giuseppe Bertone <bertone.giuseppe@gmail.com>
+// @author       Giuseppe Bertone <bertone.giuseppe@gmail.com> (https://github.com/Neurone/hus)
 // @match        http://192.168.1.1
 // @grant        none
 // @source       https://github.com/Neurone/hus
@@ -11,6 +11,7 @@
 // @updateURL    https://github.com/Neurone/hus/raw/master/resolveMacAddresses.meta.js
 // @downloadURL  https://github.com/Neurone/hus/raw/master/resolveMacAddresses.user.js
 // @license      MIT
+// @copyright    2020, Giuseppe Bertone (https://github.com/Neurone/hus)
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
